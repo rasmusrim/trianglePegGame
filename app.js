d690216e7577346c25b7e9df52eed5e7c2ee5a56
Rasmus <rasmusrim@gmail.com>
2017-12-29 19:05:23 +0100

    Updates app.js to make it clearer

--- app.js
+++ app.js
@@ -1,40 +1,23 @@
 import Board from "/modules/Board.js";
 import View from "/modules/View.js";
 
-var snapshots;
-var minPins;
+for(let i = 0; i < 100000; i++) {
 
-var tmpBoard = new Board();
+  // Set up board. First fill it with pins, and then remove the one you want.
+  var board = new Board();
+  board.fillWithPins();
+  board.removePin(0,4); // Syntax (x-coordinate, y-coordinate). The coordinates start at 0
+   
+  snapshot = board.doSimulation(); // Do the simulation and return a snapshot of the game. This snapshot can later be sent to the a view.
 
-//for(let j = 0; j < tmpBoard.positions.length; j++) {
+  let numberOfPins = board.countPins();
 
-  minPins = 10;
-
-  for(let i = 0; i < 100000; i++) {
-  
-    var board = new Board();
-    board.fillWithPins();
-    //board.removePin(tmpBoard.positions[j].x,tmpBoard.positions[j].y);
-    board.removePin(0,4);
-    
-    snapshots = board.doSimulation();
-    
-    let numberOfPins = board.countPins();
-    
-    if(numberOfPins < minPins) {
-      
-      minPins = numberOfPins;
-    }
-    
-    if(numberOfPins == 1) {
-      break;
-    }
-
+  if(numberOfPins == 1) {
+    break;
   }
-  
-  //console.log(tmpBoard.positions[j].x + ", " + tmpBoard.positions[j].y + " - " + minPins);
-//}
 
+}
 
-let view = new View(snapshots);
+// Send simulation result to view-tool to display it.
+let view = new View(snapshot);
 view.start();
